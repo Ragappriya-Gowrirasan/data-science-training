f749d437bb3fbc24dc28c55d24fdafc27e6ed634
Gowrirasan Ragappriya <gowrirasanragappriya@gmail.com>
2023-08-09 17:01:04 +0530

    update class file

---
+++ task-1/browser.py
@@ -0,0 +1,9 @@
+import sys
+import typing
+from PyQt5 import QtCore # python provided module
+from PyQt5.QtWidgets import*
+from PyQt5.QtWidgets import QWidget # build module
+#from modulename import variable or exct.....
+ #
+class MainWindow(QMainWindow): #collection of varaible and functions
+    def __init__()
