9911351765827e1b681aa2081b92d39fba491d4a
Gowrirasan Ragappriya <gowrirasanragappriya@gmail.com>
2023-08-10 14:51:50 +0530

    new example append

---
+++ task-1/basic-python/data-type/list/append.py
@@ -0,0 +1,24 @@
+#add elements to a list
+# add and remove element from a list
+#using append() function
+
+#example 01
+number =[58,32,48,58]
+print("before append in numbers: ", number)
+number.append(100)
+print("after: ", number)
+
+#exmaple 02
+letters =['p','q','r','n','s']
+print("before append in letters: ",letters)
+letters.append('a')
+print("after: ",letters)
+
+#exmaple 03: own examlpe
+word = []
+word.append('n')
+print("add new word 1: " , word)
+word.append('e')
+print("add new word 2: ",word)
+word.append('w')
+print("add new word 3: ", word)
--- task-1/basic-python/list.py
+++ task-1/basic-python/list.py
@@ -1,12 +1,47 @@
-# list animals
-a = ['dog' , 'monkey' , 'cat' , 'camel' , 'donkey']
-print(a) #print a variable into all values front order
-print(a[1]) # mention that variable
-print(a[-2]) # reverse order in value
+a1=['dog' , 'monkey' , 'cat' , 'camel' , 'donkey'] # list animales
+print(a1) # ['dog' , 'monkey' , 'cat' , 'camel' , 'donkey'] 
+print(a1[1]) # monkey
+a1[2] = 10 
+print(a1) # ['dog', 'monkey', 10, 'camel', 'donkey']
 
-#add a exchange  Value
-a[2] = 10 # cat exchange into number 10
-print(a)
+# negative indexing in python
+
+print(a1[-2]) # camel
+a1[-2] = 8
+print(a1) # [dog , monkey , 10 , 8 ,donkey]
+
+
+#slicing of a list
+
+x = ['mango' , 'banana' , 'pineapple' , 'apple' , 'papaya']
+print(x) # ['mango' , 'banana' , 'pineapple' , 'apple' , 'papaya']
+print(x[1:4]) # ['banana' , 'pineapple' , 'apple']
+print(x[-1:-3]) # []
+print(x[1:3]) # ['babana' , 'pineapple']
+print(x[-1]) #papaya
+print(x[-1:2]) # []
+print(x[3:-1]) #['apple']
+print(x[-2:-4]) #[]
+
+x[1:4] == x[1:3]
+print(x) # ['mango', 'banana', 'pineapple', 'apple', 'papaya']
+
+
+letter = ['a','b','c','d','e','f','g','h']
+print(letter[2:6]) #['c', 'd', 'e', 'f']
+print(letter[3:]) #['d', 'e', 'f', 'g', 'h']
+print(letter[-2:]) #['g', 'h']
+
+
+
+
+
+
+
+
+
+
+
 
 
 
