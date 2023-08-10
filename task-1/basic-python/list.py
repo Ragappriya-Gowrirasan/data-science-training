516b928a634822f128a48384b02b063a0225c0b5
Gowrirasan Ragappriya <gowrirasanragappriya@gmail.com>
2023-08-10 11:18:30 +0530

    add a list file

---
+++ task-1/basic-python/list.py
@@ -0,0 +1,14 @@
+# list animals
+a = ['dog' , 'monkey' , 'cat' , 'camel' , 'donkey']
+print(a) #print a variable into all values front order
+print(a[1]) # mention that variable
+print(a[-2]) # reverse order in value
+
+#add a exchange  Value
+a[2] = 10 # cat exchange into number 10
+print(a)
+
+
+
+
+
--- task-1/browser/myclass.py
+++ task-1/browser/myclass.py
@@ -1,6 +1,9 @@
 class myclass1:
+    a=10
     def myfunction(self):
         print("this is myclass")
 object=myclass1()
 object=myfunction()
+value=object.a
+print("valuse: "+value)
 
