802ef25854d8935a8c9cf669532659118b20cf60
Gowrirasan Ragappriya <gowrirasanragappriya@gmail.com>
2023-08-10 15:13:01 +0530

    update extend list

--- task-1/basic-python/data-type/list/append.py
+++ task-1/basic-python/data-type/list/append.py
@@ -1,6 +1,7 @@
 #add elements to a list
 # add and remove element from a list
 #using append() function
+#list_name.append()
 
 #example 01
 number =[58,32,48,58]
@@ -22,3 +23,16 @@ word.append('e')
 print("add new word 2: ",word)
 word.append('w')
 print("add new word 3: ", word)
+
+#exmaple 04
+#random =[]
+#random.append('priya',5) 
+#print("numbers or names: ",random)
+#append takes one argunment 
+
+#exmaple 5
+number = [2,5,8,1,0,9]
+letters =['r','n','s','h']
+number.append(letters)
+print("different list: ",number)
+#[2, 5, 8, 1, 0, 9, ['r', 'n', 's', 'h']]
---
+++ task-1/basic-python/data-type/list/extend.py
@@ -0,0 +1,9 @@
+#use the extend method to all items of an iterable
+#List_name.extend(another_list_name)
+
+#exmaple 1
+number = [2,5,8,1,0,9]
+letters =['r','n','s','h']
+number.extend(letters)
+print("different list: ",number)
+#
