ca88dea6fc0eef193caeecaedf1eadaffedb648b
Gowrirasan Ragappriya <gowrirasanragappriya@gmail.com>
2023-08-11 12:17:53 +0530

    update remove file

---
+++ task-1/basic-python/data-type/list/remove-items-list/remove.py
@@ -0,0 +1,11 @@
+#use the remove method to delete a list item
+#list_name.remove(list_item)
+name = ['priya','harish','varsha','nilany','thusha','roshi']
+print(name) #['priya', 'harish', 'varsha', 'nilany', 'thusha', 'roshi']
+name.remove('thusha')
+print(name) #['priya', 'harish', 'varsha', 'nilany', 'roshi']
+name.remove('varsha')
+print(name)
+
+
+
