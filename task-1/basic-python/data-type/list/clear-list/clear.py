56a9772a836bf91be4fc59983c4abeca4234b362
Gowrirasan Ragappriya <gowrirasanragappriya@gmail.com>
2023-08-11 16:26:53 +0530

    update clear file

--- task-1/basic-python/data-type/list/clear-list/clear.py
+++ task-1/basic-python/data-type/list/clear-list/clear.py
@@ -4,8 +4,19 @@ number.clear()
 print(number) #[]
 
 #example
-names = ['kali','nali','thusha','risha']
-names.clear('kali')
-print(names)
+#names = ['kali','nali','thusha','risha']
+#names.clear('kali')
+#print(names)
+
+
+#example own idea but not method
+letters = ['r','s','t','u','v']
+numbers = [1,3,5,7,9,8]
+name = ['rusha','nihara','pavi','niththi']
+list =[letters,numbers,name]
+print(list) #[['r', 's', 't', 'u', 'v'], [1, 3, 5, 7, 9, 8], ['rusha', 'nihara', 'pavi', 'niththi']]
+list.clear()
+print(list)
+#
 
 
