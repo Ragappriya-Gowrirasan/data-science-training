7bfa53bce1de65c67267e88f891275db3792e00a
Gowrirasan Ragappriya <gowrirasanragappriya@gmail.com>
2023-08-11 11:34:43 +0530

    add insert file

---
+++ task-1/basic-python/data-type/list/insert.py
@@ -0,0 +1,16 @@
+#add on element at the specified index
+#insert()
+number = [10 , 20 , 50, 15]
+number.insert(4 , 90)
+print(number)
+
+#exmaple 01
+name = []
+name.insert(0 , 'n')
+print(name) # print letter: ['n']
+name.insert(1,'a')
+print(name) #print letter: ['n', 'a']
+name.insert(2,'m')
+print(name) #print letter: ['n', 'a', 'm']
+name.insert(3,'e')
+print(name) #print letter: ['n', 'a', 'm', 'e'] 
