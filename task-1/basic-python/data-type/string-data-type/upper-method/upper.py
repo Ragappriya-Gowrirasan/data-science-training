b03177636287c32af518631934e81a411024dad3
Gowrirasan Ragappriya <gowrirasanragappriya@gmail.com>
2023-08-14 12:13:51 +0530

    add string-upper page

---
+++ task-1/basic-python/data-type/string-data-type/upper-method/upper.py
@@ -0,0 +1,9 @@
+# method converts all upper characters in a string into 
+# list_name.upper()
+names = 'my name priya'
+print(names.upper())
+
+#example 1
+sentence = """hello! i'm priya. i'm 23 years."""
+print(sentence.upper())
+
