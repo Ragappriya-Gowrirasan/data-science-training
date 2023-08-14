5599b6579e2694797669973e44d0faa0a73810b9
Gowrirasan Ragappriya <gowrirasanragappriya@gmail.com>
2023-08-14 12:25:33 +0530

    update string-lower page

---
+++ task-1/basic-python/data-type/string-data-type/lower-method/lower.py
@@ -0,0 +1,17 @@
+# method converst all uppercase characters in a into lowercase characters
+# list_name.lower()
+name = 'PYTHON'
+print(name.lower())
+
+#example
+subjects = "COMmeRcE, tamIL, eNgliSh"
+print(subjects.lower())
+
+# example
+list1 = "PYTHON"
+list2 = "JAVA"
+
+if(list1.lower()==list2.lower()):
+    print('this is same list')
+else:
+    print('this is not same list')
