ef484f17fe0b3aa7be377af294391fb22adfba0a
Gowrirasan Ragappriya <gowrirasanragappriya@gmail.com>
2023-08-15 14:34:15 +0530

    add string-center page

---
+++ task-1/basic-python/data-type/string-data-type/center-method/center.py
@@ -0,0 +1,10 @@
+# method return a new centered string after padding it with the specified character
+words = "python is language"
+print(words.center(58))
+print(words.capitalize())
+
+#example 1
+print('enter your friend name: ')
+random = input()
+random1 = random.capitalize()
+print(random1.center(58))
