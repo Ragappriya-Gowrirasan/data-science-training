5437ae88752f126aa5992b92dd13fd5379725fac
Gowrirasan Ragappriya <gowrirasanragappriya@gmail.com>
2023-08-17 14:10:17 +0530

    add string identifier page

---
+++ task-1/basic-python/data-type/string-data-type/identifier-method/isidentifier.py
@@ -0,0 +1,17 @@
+# example 1
+text = 'python'
+print(text.isidentifier())
+
+# example 2
+print('type a text: ')
+text1 = input()
+print(text1.isidentifier())
+
+
+#example 3
+text2 = 'variable1254353'
+print(text.isidentifier())
+
+# example 4
+text3 = ' hello'
+print(text3.isidentifier())
