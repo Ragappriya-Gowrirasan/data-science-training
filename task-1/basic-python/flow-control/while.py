97b4bab0c1a3196971bc2fe03f85fb8eaafc427e
Gowrirasan Ragappriya <gowrirasanragappriya@gmail.com>
2023-08-23 16:13:04 +0530

    feat: add while loop page

---
+++ task-1/basic-python/flow-control/while.py
@@ -0,0 +1,28 @@
+
+# print hello 5 times
+count = 1
+while count <= 5:
+    print('hello')
+    count += 1
+
+# 10 times print welcome and same time hello print 5 times
+# nested while loop
+a = 1
+while a <= 5:
+    print('hello')
+    b = 1
+    while b <= 10:
+        print('welcome')
+        b += 1
+    a += 1
+
+# 
+counter = 1
+while counter <=10:
+    print(counter)
+    counter = counter + 2
+
+c = 0
+while True:
+    print('hello')
+    
