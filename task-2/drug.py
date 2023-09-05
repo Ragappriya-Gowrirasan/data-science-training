faa973ba6bf5e81fb7c50fb61b8c24011405a627
Gowrirasan Ragappriya <gowrirasanragappriya@gmail.com>
2023-09-05 10:02:21 +0530

    feat: add drug page

--- task-2/drug.py
+++ task-2/drug.py
@@ -1,12 +1,50 @@
-# import library packages
-# http request library_requests
+#HTTP request 
 import requests
 from bs4 import BeautifulSoup
+# CSV file open import
+import pandas as pd
 
-# open url link of website
-url = "https://www.webmd.com/search/search_results/default.aspx?query=atorvastatin"
-response = requests.get(url).content
-#text read using beatufulsoup
-b = BeautifulSoup(response,"html.parser") 
-table =b.find_all("div",{"class":"main-container main-container-2"})
-print(table)
+# Send an HTTP request and get the page content
+url = "https://reviews.webmd.com/drugs/drugreview-841-atorvastatin-oral"
+response = requests.get(url)
+
+if response.status_code == 200:
+    page_content = response.text
+else:
+    print("Failed to retrieve the web page. Status code:", response.status_code)
+    exit()
+
+# Parse the HTML content with BeautifulSoup
+soup = BeautifulSoup(page_content, 'html.parser')
+
+# Extract data from the web page
+reviews = []
+ratings = []
+
+for review in soup.find_all("div", class_="shared-reviews-container"):
+    # Extract review text
+    review_text = review.find("div", class_="review-details")
+    if review_text:
+        reviews.append(review_text.text.strip())
+    else:
+        reviews.append("N/A")
+
+    # Extract rating
+    rating = review.find("div", class_="overall-rating.name")
+    if rating:
+        ratings.append(rating.text.strip())
+    else:
+        ratings.append("N/A")
+
+# Create a DataFrame
+data = {"Review": reviews, "Rating": ratings}
+df = pd.DataFrame(data)
+
+# Export data to a CSV file
+df.to_csv("webmd_reviews.csv", index=False)
+
+
+
+
+
+
